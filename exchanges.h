f8391487867c5b55592537d8defd4f449ec3396e
tstrube <76815855+tstrube@users.noreply.github.com>
2021-09-25 01:51:49 +0200

    removed untested exchanges

--- exchanges.h
+++ exchanges.h
@@ -1,57 +1,4 @@
-
 struct exchange_settings {
-  char* host;
-  int   port;
-  char* url;
-  char* wsproto;
-  char* subscribe;
-};
-
-// bitstamp USD BTC tested & working
-
-const exchange_settings bitstampUSDBTC = {
-  "ws.pusherapp.com",
-  80,
-  "/app/de504dc5763aeef9ff52?protocol=7",
-  "pusher",
-  "{\"event\": \"pusher:subscribe\", \"data\": {\"channel\": \"live_trades\"}}"
-};
-
-// UNTESTED
-
-const exchange_settings bitstampEURBTC = {
-  "ws.pusherapp.com",
-  80,
-  "/app/de504dc5763aeef9ff52",
-  "pusher",
-  "{\"event\": \"pusher:subscribe\", \"data\": {\"channel\": \"live_trades_btceur\"}}"
-};
-
-// UNTESTED
-
-const exchange_settings gdaxUSDBTC = {
-  "ws-feed-public.sandbox.gdax.com",
-  80,
-  "/",
-  "websocket",
-  "{\"type\": \"subscribe\", \"channels\": [{\"name\": \"ticker\",\"product_ids\": [\"BTC-USD\"]}]}"
-};
-
-// new UNTESTED
-
-const exchange_settings bitfinexUSDBTC = {
-  "api.bitfinex.com",
-  443,
-  "/ws/2",
-  "websocket",
-  "{\"event\":\"subscribe\",\"channel\":\"trades\",\"symbol\":\"tBTCUSD\" }"
-};
-
-
-
-// New exchanges listening to fetchPairs setting
-
-struct exchange_settings_opt {
   char* host;
   int   port;
   char* url;
@@ -61,7 +8,7 @@ struct exchange_settings_opt {
   char* separator;
 };
 
-const exchange_settings_opt okex = {
+const exchange_settings okex = {
   "real.okex.com",
   443,
   "/ws/v3",
